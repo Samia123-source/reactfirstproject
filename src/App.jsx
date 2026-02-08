d7caf0ec7258b27dca9593df96f1ff367e8ac5e1
Samia123-source <samiamahim04@gmail.com>
2026-02-08 17:57:39 +0600

    ok

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Vite</h1> 
+       <Person></Person>
+       <Student></Student>
+       <Developer></Developer>
+    </>
+  )
+}
+
+function Person() {
+  const age = 25;
+  const money = 20;
+  const person = {name: "sakib", age: 12}
+  return <h3>I am {person.name} with age:{age+money}</h3>
+}
+function Student() {
+  return (
+     <div className= "student"> 
+    <h3>This is a Student</h3>
+    <p>Name:</p>
+    <p>Age:</p>
+  </div>
+  )
+ 
+}
+function Developer() {
+  const developerstyle= {
+    margin: '20px',
+    padding: '20px',
+    border:'2px solid purple',
+    borderRadius: '20px'
+  }
+  return(
+  <div style= {developerstyle}>
+    <h5>Devo devo</h5>
+    <p>Coding: </p>
+  </div>
+  )
+}
+
+
+export default App
