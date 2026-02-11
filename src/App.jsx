1458e6de2194c48a2721b8a46484d2f73f7f9a33
Samia123-source <samiamahim04@gmail.com>
2026-02-11 23:23:34 +0600

    rendering lists of users using map

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 
 import './App.css'
 import Actor from './Actor'
+import Singer from './Singer'
 
 
 function App() {
 
-  const actors = ['Sakib', 'Shariful Raj', 'Jasim', 'Rubel']
+  const actors = ['Sakib', 'Shariful Raj', 'Jasim', 'Rubel'];
+  const singers = [
+    {id:1, name:'Dr.Mahfuz', age: 68},
+    {id:2, name: 'Eva', age: 38},
+    {id:3, name:'Shuvro',age:58},
+    {id:4, name: 'Pritom',age:28}
+    ]
 
   return (
     <>
 
       <h3>Vite+React</h3> 
-      <Actor name= {"Bappa Raj"}></Actor>
+      {
+        singers.map(singer => <Singer singer={singer}></Singer>)
+      }
+      <Actor name={"Bappa Raj"}></Actor>
     
      {
       actors.map (actor => <Actor name={actor}></Actor>)
@@ -23,3 +33,5 @@ function App() {
 
 
  export default App
+// ekhane Singer j class ta banaiso , oitay 2 ta jinish proyojon , name r age , so , 
+// 2 tai pathaite hobe 
